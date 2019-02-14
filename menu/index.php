b9318abb385c4e87a65de42c6176a2eb0ea8ac04
Anna Karutina <anna.karutina@khk.ee>
2019-02-14 09:04:51 +0200

    oop menu - loodud rakenduse peamall ja täidetud test sisuga

--- menu/index.php
+++ menu/index.php
@@ -1,16 +1,16 @@
 <?php
-require_once 'lib/fnk.php';
-require_once 'menus/menu.php';
-// lehe sisu
-// päis - header ja muud vajalikud osad
-pais();
-// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
-menu(array(
-    'praed' => $praed,
-    'supid' => $supid,
-    'magus' => $magusad,
-    'joogid' => $joogid
-));
-// jalus - kõik JS asjad
-jalus();
-?>
+////tegelikult peavad olema conf.php failis
+define('BASE_DIR', './'); // define('BASE_DIR', '../');
+require_once(BASE_DIR.'conf.php');
+//
+/*echo '<pre>';
+print_r($sess);
+echo '</pre>';*/
+
+$mainTmpl = new Template('main');
+
+$mainTmpl->set('title', 'Menu Application');
+$mainTmpl->set('page title', 'Tartu KHK Menu');
+
+$mainTmplContent = $mainTmpl->parse();
+echo $mainTmplContent;
