3486d72201055605fe26f238436f1819fae5f051
Anna Karutina <anna.karutina@khk.ee>
2019-02-21 09:03:31 +0200

    oop menu - täiendatud söögi andmed uue massiivina

--- menu/index.php
+++ menu/index.php
@@ -17,11 +17,42 @@ $courseCardTmpl = new Template('course_card');
 $courseCardHeaderTmpl = new Template('course_card_header');
 $courseCardDataTmpl = new Template('course_card_data');
 
+$courseDatalistTmpl = new Template('course_data_list');
+
 $courseNames = array(
-    'praed' => 'fa-utensils',
-    'supid' => 'fa-utensil-spoon',
-    'magustoidud' => 'fa-cookie-bite',
-    'joogid' => 'fa-glass-whiskey');
+    array(
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'dish_name' => 'Sealihapada ploomide ja aprikoosiga',
+                'dish_description' => 'sealihapada, lisand, salat, leib',
+                'dish_price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'dish_name' => 'Praetud kanakints',
+                'dish_description' => 'praetud kana, lisand, salat, leib',
+                'dish_price' => 2.50,
+                'discount' => 2.13
+            )
+        )
+    ),
+    array(
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'dish_name' => 'Rassolnik',
+                'dish_description' => 'supp, hapukoor, leib',
+                'dish_price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
+);
+
+
 
 foreach ($courseNames as $courseName => $courseIcon){
     $courseCardHeaderTmpl->set('course_name', $courseName);
@@ -29,7 +60,11 @@ foreach ($courseNames as $courseName => $courseIcon){
     $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
 
     $courseCardDataTmpl->set('course_name', $courseName);
+    $courseCardDataTmpl->set('course_data_list', $courseDatalistTmpl->parse());
     $courseCardTmpl->set('course_card_data', $courseCardDataTmpl->parse());
+
+
+
     $contentTmpl->add('course_cards', $courseCardTmpl->parse());
 }
 
